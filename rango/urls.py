b83fd75a447e0018550e3246689e0343516e0407
jerry <668Jerry@gmail.com>
2014-07-02 01:43:56 +0800

    7.2.7Cleaner Forms Finished

--- rango/urls.py
+++ rango/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import patterns, url
 from rango import views
 
-from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-
-from django.conf.urls import patterns, url
-from rango import views
-from django.conf import settings
-
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     # url(r'^about/$', views.about, name='about'),
