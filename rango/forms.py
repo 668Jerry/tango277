b449f6623535f2eb7ca3676a817f4b542682f472
jerry <668Jerry@gmail.com>
2014-07-03 14:54:15 +0800

    8.4.1 finished

--- rango/forms.py
+++ rango/forms.py
@@ -36,3 +36,19 @@ class PageForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+from rango.models import UserProfile
+from django.contrib.auth.models import User
+from django import forms
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
