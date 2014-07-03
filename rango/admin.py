483d3ccf11effc5fccc64cbff0f426beb66c2765
jerry <668Jerry@gmail.com>
2014-07-03 14:49:26 +0800

    8.3 finished

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 admin.site.register(Category)
 admin.site.register(Page)
+admin.site.register(UserProfile)
