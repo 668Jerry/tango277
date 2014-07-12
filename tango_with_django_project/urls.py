e789a7fa14b010a89ed98c1fa2d4be29329de2a3
jerry <668Jerry@gmail.com>
2014-07-13 05:43:12 +0800

    15 finished

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,8 +15,6 @@ urlpatterns = patterns('',
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-
-
     url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
     url(r'^category/(?P<category_name_url>\w+)/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_url>\w+)/$', views.category, name='category'),  # New!
@@ -31,6 +29,7 @@ urlpatterns = patterns('',
     url(r'^goto/$', views.track_url, name='track_url'),
     url(r'^like_category/$', views.like_category, name='like_category'),
     url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
+    url(r'^auto_add_page/$', views.auto_add_page, name='auto_add_page'),
 )
 
 urlpatterns += patterns('',
