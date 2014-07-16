f8462f621285805b4bf315725174b59877c748ce
jerry <668Jerry@gmail.com>
2014-07-16 11:53:58 +0800

    716javascript random number before fb login

--- rango/forms.py
+++ rango/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
@@ -57,3 +61,33 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
+
+class DocumentForm(forms.Form):
+    docfile = forms.FileField(
+        label='Select a file'
+    )
+
+from rango.models import Document
+from rango.forms import DocumentForm
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('myproject.myapp.views.list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render_to_response(
+        'myapp/list.html',
+        {'documents': documents, 'form': form},
+        context_instance=RequestContext(request)
+    )
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -30,6 +30,7 @@ urlpatterns = patterns('',
     url(r'^like_category/$', views.like_category, name='like_category'),
     url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
     url(r'^auto_add_page/$', views.auto_add_page, name='auto_add_page'),
+    url(r'^list/$', 'list', name='list'),
 )
 
 urlpatterns += patterns('',
